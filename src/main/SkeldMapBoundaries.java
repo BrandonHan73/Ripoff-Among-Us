8f4f344c3164068fb75bcd64a89bad28943b6352
Brandon Han <brandontthan2005@gmail.com>
2020-11-25 11:26:33 -0800

    High slope lines work

--- src/main/SkeldMapBoundaries.java
+++ src/main/SkeldMapBoundaries.java
@@ -18,6 +18,8 @@ public class SkeldMapBoundaries {
         coords.add(new Coordinate(3282, 223));
         coords.add(new Coordinate(4277, 221));
         coords.add(new Coordinate(4730, 673));
+        coords.add(new Coordinate(4731, 917));
+        coords.add(new Coordinate(5189, 925));
 
     }
 
@@ -32,10 +34,22 @@ public class SkeldMapBoundaries {
         for(int i = 0; i < coords.size() - 1; i++) {
             Coordinate c1 = coords.get(i);
             Coordinate c2 = coords.get(i + 1);
+            boolean vertical = false;
+            double m = 0;
             if(c1.getX() != c2.getX()) {
-                double m = (c1.getY() - c2.getY()) / (double)(c1.getX() - c2.getX());
+                m = (c1.getY() - c2.getY()) / (double)(c1.getX() - c2.getX());
+
+            } else vertical = true;
+            if((Math.abs(m) <= 1) && !vertical) {
                 for(int j = c1.getX(); j != c2.getX(); j += (c2.getX() - c1.getX()) / Math.abs((c1.getX() - c2.getX()))) {
-                    map[j][(int)f(m, c1.getX(), c1.getY(), j)] = true;
+                    map[j][(int) f(m, c1.getX(), c1.getY(), j)] = true;
+
+                }
+
+            } else {
+                if(!vertical) m = 1 / m;
+                for(int j = c1.getY(); j != c2.getY(); j += (c2.getY() - c1.getY()) / Math.abs((c1.getY() - c2.getY()))) {
+                    map[(int) f(m, c1.getY(), c1.getX(), j)][j] = true;
 
                 }
 
